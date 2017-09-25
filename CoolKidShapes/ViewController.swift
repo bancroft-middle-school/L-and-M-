747e1a138abba153167b9ae80d6f48a73d00efda
Bancroft <maaniceto@aapl000617.local>
2017-09-25 09:46:50 -0400

    Initial Commit

---
+++ CoolKidShapes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolKidShapes
+//
+//  Created by Maia Aniceto on 9/25/17.
+//  Copyright © 2017 L&M Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoolKidShapesTests/CoolKidShapesTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoolKidShapesTests.swift
+//  CoolKidShapesTests
+//
+//  Created by Maia Aniceto on 9/25/17.
+//  Copyright © 2017 L&M Corp. All rights reserved.
+//
+
+import XCTest
+@testable import CoolKidShapes
+
+class CoolKidShapesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
