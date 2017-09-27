c6664a26a38483f227c8ef31c10649a7792eeee8
Bancroft <maaniceto@aapl000617.local>
2017-09-27 09:16:34 -0400

    Hello

--- CoolKidShapes/ViewController.swift
+++ CoolKidShapes/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label1.text = "Are you ready?"
+   
+    }
+    
+    @IBOutlet weak var label1: UILabel!
+
+    
+    @IBAction func buttonplay(_ sender: Any) {
+        label1.text = "Let's Go!"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-}
-
