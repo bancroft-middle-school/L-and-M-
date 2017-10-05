157025171f5163f2e8e2cad4b673204a9b7c59e7
Bancroft <maaniceto@aapl000617.local>
2017-10-05 10:40:20 -0400

    We added the arrays

--- CoolKidShapes/ViewController.swift
+++ CoolKidShapes/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var questions = ["What is this shape?"]
+    
+    var answers = [["Cicle"]]
+    
+    @IBOutlet weak var buttonplay: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         label1.text = "What is this shape?"
-   
+    
     }
     
     @IBOutlet weak var label1: UILabel!
@@ -27,6 +35,13 @@ class ViewController: UIViewController {
     }
     @IBAction func button3(_ sender: Any) {
         label1.text = "Incorrect! Try again"
-    }
 
     }
+    
+    
+
+}
+
+      
+
+
